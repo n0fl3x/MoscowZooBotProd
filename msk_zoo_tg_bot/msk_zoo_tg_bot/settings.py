337d16a32afbbdbc985b76534f951344eeef8d4e
Roman Nevzorov <pomkanevzorov@gmail.com>
2023-09-11 18:18:18 +0600

    Switched debug.

--- msk_zoo_tg_bot/msk_zoo_tg_bot/settings.py
+++ msk_zoo_tg_bot/msk_zoo_tg_bot/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'moskovskiyzooparkbot.pythonanywhere.com',
